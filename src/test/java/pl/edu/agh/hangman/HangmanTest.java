9d5d3760f3ee635c23c059dfa442d93bb9df3757
Wojciech Frącz <fraczwojciech@gmail.com>
2020-02-16 13:35:04 +0100

    Sample test

---
+++ src/test/java/pl/edu/agh/hangman/HangmanTest.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.hangman;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HangmanTest {
+    @Test
+    public void testThatTestsNothing() {
+        Assert.assertTrue("You need to implement it!", false);
+    }
+}
