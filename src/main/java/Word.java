f0e37ff551fa1763428ba57edf8d40723b7b4ae3
Grzegorz Koziel <kniadziu@gmail.com>
2020-03-01 14:53:50 +0100

    Random file - zwraca losowe slowo

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -5,66 +5,57 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Word {
+    private int maxLength;
+
+    public Word(int maxLength) {
+        this.maxLength = maxLength;
+    }
 
     public Word() {
     }
 
-    public   String getFile(String txtFile) {
+    public String getFile(String txtFile) {
 
-                //txtFile = "src/main/resources/slowa.txt";
-                BufferedReader br = null;
-                String line = "";
-                ArrayList<String> wordTable = new ArrayList<>();
+        //txtFile = "src/main/resources/slowa.txt";
+        BufferedReader br = null;
+        String line = "";
+        ArrayList<String> wordTable = new ArrayList<>();
 
+        try {
+
+            br = new BufferedReader(new FileReader(txtFile));
+            while ((line = br.readLine()) != null) {
+                if (line.length() <= maxLength) {
+                    wordTable.add(line);
+                }
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
                 try {
-
-                    br = new BufferedReader(new FileReader(txtFile));
-
-
-                    while ((line = br.readLine()) != null) {
-                       // String[] word = line.split(" \\| ");
-                        wordTable.add(line);
-                    }
-
-
-                } catch (FileNotFoundException e) {
-                    System.out.println("File not found");
+                    br.close();
                 } catch (IOException e) {
                     e.printStackTrace();
-                } finally {
-                    if (br != null) {
-                        try {
-                            br.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
                 }
+            }
+        }
 
-//        for (String s: wordTable){
-//            System.out.println(s);
-//        }
-
-      //  ArrayList<String> wordTable = getFile();
         Random random = new Random();
 
         int randomIndex = (int) (Math.random() * wordTable.size());
-       // System.out.println( "Random word: " +  wordTable.get( randomIndex ) );
-        return wordTable.get( randomIndex);
+        // System.out.println( "Random word: " +  wordTable.get( randomIndex ) );
+        return wordTable.get(randomIndex);
     }
 
-
-    public static void main(String[] args) {
-        Word s = new Word();
-        System.out.println(s.getFile("src/main/resources/slowa.txt"));
-    }
-
-//    public String getWord(){
-//        ArrayList<String> wordTable = getFile();
-//        Random random = new Random();
-//
-//        int randomIndex = (int) (Math.random() * wordTable.size());
-//        System.out.println( "Random Color: " +  wordTable.get( randomIndex ) );
-//        return wordTable.get( randomIndex);
+//Wywyolanie klasy w main
+//    public static void main(String[] args) {
+//        Word s = new Word(10);
+//        System.out.println(s.getFile("src/main/resources/slowa.txt"));
 //    }
+
 }
