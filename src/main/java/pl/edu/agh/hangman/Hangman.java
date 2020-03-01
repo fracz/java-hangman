68bb5bdabdd8edbad4738575868dde6a67ae934c
MichalJalowik <michaljalowik@gmail.com>
2020-03-01 15:17:06 +0100

    Uruchomiona funkcjonalnosc podawanie litery(set actual word) i wyswietlanie aktualnego slowa

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -2,6 +2,15 @@ package pl.edu.agh.hangman;
 
 public class Hangman {
 
+    private String word;
+    private String[] wordIntoTable = new String[]{};
+    private String[] actualWord = new String[]{};
+
+    public Hangman(String word){
+        this.word = word;
+    }
+
+
     public static final String[] HANGMANPICS = new String[]{
             "  +---+\n" +
                     "  |   |\n" +
@@ -54,6 +63,100 @@ public class Hangman {
                     "========"
     };
 
-    public static void main(String[] args) {
+    public void showHangmanpics (){
+
+        for (String hangPic: HANGMANPICS) {
+            System.out.println(hangPic);
+        }
+
     }
+
+    public void getWord(){
+        System.out.println(word);
+    }
+
+
+
+    public void convertWordIntoTable (){
+
+        this.wordIntoTable = new String[word.length()];
+        this.actualWord = new String[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            actualWord[i] = " _ ";
+        }
+
+
+        for (int i = 0; i < word.length(); i++) {
+            wordIntoTable[i] = word.substring(i,i+1);
+        }
+
+
+    }
+
+    public void getWordTable(){
+
+        for (String letter: wordIntoTable) {
+            System.out.print(letter.toLowerCase());
+        }
+        System.out.println("");
+
+    }
+
+
+    public void showHiddenWord(){
+
+        for (String letter: wordIntoTable) {
+            System.out.print(" _ ");
+        }
+        System.out.println("");
+
+    }
+
+    public void giveLetter(String guessLetter){
+
+        for (int i = 0; i < wordIntoTable.length ; i++) {
+
+            if (guessLetter.equals(wordIntoTable[i])){
+                System.out.print(wordIntoTable[i]);
+            }else{
+                System.out.print(" _ ");
+            }
+        }
+
+
+        System.out.println("");
+
+    }
+
+
+
+    public void setActualWord (String guessLetter){
+
+        for (int i = 0; i < word.length(); i++) {
+
+            if (guessLetter.equals(wordIntoTable[i])){
+                actualWord[i] = word.substring(i,i+1);
+
+            }if(!actualWord[i].equals(" _ ")) {
+
+            } else{
+                actualWord[i] = " _ ";
+            }
+        }
+        getActualWord();
+
+    }
+
+    public void getActualWord(){
+
+        for (String letter: actualWord) {
+            System.out.print(letter.toLowerCase());
+        }
+        System.out.println("");
+
+    }
+
+
+
 }
---
+++ src/main/java/pl/edu/agh/hangman/HangmanDEMO.java
@@ -0,0 +1,24 @@
+package pl.edu.agh.hangman;
+
+public class HangmanDEMO {
+
+    public static void main(String[] args) {
+
+        Hangman hangman = new Hangman("zaDecia java");
+        hangman.showHangmanpics();
+        //hangman.getWord();
+        //hangman.convertWordIntoTable();
+        //hangman.getWordTable();
+        //hangman.showHiddenWord();
+        //hangman.giveLetter("a");
+        //hangman.giveLetter("z");
+        hangman.convertWordIntoTable();
+        hangman.setActualWord("a");
+        hangman.setActualWord("z");
+        hangman.setActualWord("j");
+        hangman.setActualWord("i");
+        hangman.setActualWord("e");
+
+    }
+
+}
