f6e8bd9aaf7bcc31f3767eae90f8fd5b51624ebf
Grzegorz Nawrot <nawrot@student.agh.edu.pl>
2021-01-23 11:23:07 +0100

    Commit of writer class update

--- src/main/java/pl/edu/agh/hangman/HangmanWriter.java
+++ src/main/java/pl/edu/agh/hangman/HangmanWriter.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 
 public class HangmanWriter {
-	public static void main(String args[]) {
+	public static void (String args[]) {
 	
 	Scanner guess = new guess(System.In);
 	System.out.print("Enter word:")
