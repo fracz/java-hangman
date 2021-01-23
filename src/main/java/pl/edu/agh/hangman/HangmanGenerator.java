f39de645981632533a33e7161113e99cea275702
Marcin Kubasiak <kubasiak@student.agh.edu.pl>
2021-01-23 10:28:28 +0100

    Commit of HangmanGenerator class

--- src/main/java/pl/edu/agh/hangman/HangmanGenerator.java
+++ src/main/java/pl/edu/agh/hangman/HangmanGenerator.java
@@ -1,7 +1,20 @@
 package pl.edu.agh.hangman;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class HangmanGenerator {
 
 	// klasa odpowiedzialna za wczytywanie slowa z pliku
 	// file reader
+
+	public static void appendWordsToList(String path) throws IOException {
+
+		List<String> words = Files.readAllLines(Paths.get("/src/main/resources/slowa.txt"));
+
+	}
+
 }
