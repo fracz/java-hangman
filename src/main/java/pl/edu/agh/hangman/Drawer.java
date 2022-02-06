e3ee2964311301cded48ad97db25b2d716d8d1f6
Mikołaj Dudek <mikolaj1dudek@gmail.com>
2022-02-06 10:20:57 +0100

    Drawer added

---
+++ src/main/java/pl/edu/agh/hangman/Drawer.java
@@ -0,0 +1,26 @@
+package pl.edu.agh.hangman;
+
+import java.util.Scanner;
+
+import static pl.edu.agh.hangman.Hangman.HANGMANPICS;
+
+public class Drawer {
+
+    Scanner scanner = new Scanner(System.in);
+    
+
+
+    public void drawGameboard(String gameBoard, int lives){
+
+        System.out.println(gameBoard);
+        System.out.println(HANGMANPICS.length- lives-1);
+
+    }
+
+    public char userInput(){
+        char inputChar = scanner.next().charAt(0);
+        return inputChar;
+
+    }
+
+}
