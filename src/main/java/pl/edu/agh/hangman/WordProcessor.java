58da0120ade92d37e67c08d3bc44d548745b0b8c
Artur <artur.sobieraj126@gmail.com>
2022-02-06 13:24:02 +0100

    Core classes

---
+++ src/main/java/pl/edu/agh/hangman/WordProcessor.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.hangman;
+
+/**
+ * Klasa powinna zawierać metody odpowiadające za:
+ * - losowanie  słowa
+ * - sprawdzanie czy dana litera jest w wylosowanym słowie
+ */
+public class WordProcessor {
+}
