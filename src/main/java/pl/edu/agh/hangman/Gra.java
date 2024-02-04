5d2cd7d0fa0a7c332962287ad2889a4873fdcae8
msiewierska <marta.siewierska-stanczak@creativestyle.pl>
2024-02-04 15:38:39 +0100

    Implementa game logic

--- src/main/java/pl/edu/agh/hangman/Gra.java
+++ src/main/java/pl/edu/agh/hangman/Gra.java
@@ -1,11 +1,17 @@
 package pl.edu.agh.hangman;
 
 import java.util.ArrayList;
-
+import java.util.Locale;
+import java.util.Scanner;
 public class Gra{
     public String word;
 
-   private ArrayList<String> attempts = new ArrayList<>();
+    public boolean gameIsWon = false;
+    private int counter = 0;
+
+   private ArrayList<String> attempts = new ArrayList<>(); // lista wprowadzonych liter
+    private ArrayList<String> guessedLetters = new ArrayList<>(); // lista odgadniętych liter
+
     public static final String[] HANGMANPICS = new String[]{
             "  +---+\n" +
                     "  |   |\n" +
@@ -61,14 +67,96 @@ public class Gra{
         this.word=word;
 
     }
-   public void start(){
-printState();
+   public void start() {
+       System.out.println("Zagraj w wisielca");
+       printState();
 
+       while (attempts.size() < 6 && !gameIsWon) {
+            performRound();
+        }
+
+       if (!gameIsWon) {
+           System.out.println("");
+           System.out.println("Przegrałeś");
+           System.out.println("Czy chcesz spróbować jeszcze raz? Wpisz tak");
+
+       }
    }
-private void printState(){
-   int numberOfAttempts=attempts.size();
-   System.out.println(HANGMANPICS[numberOfAttempts]);
-}
 
+   private void performRound() {
+       String letter = askForInput();
 
+       if (word.contains(letter)) {
+           guessedLetters.add(letter);
+           System.out.println("Litera znajduje się w słowie");
+       } else {
+           System.out.println("Litery nie ma w słowie");
+           attempts.add(letter);
+           counter++;
+       }
+
+       if (checkIfGameIsWon()) {
+           gameIsWon = true;
+           System.out.println("Wygrałęś");
+       }
+       printState();
+   }
+
+    private boolean checkIfGameIsWon() {
+        boolean gameIsWon = true;
+        ArrayList<String> letters = new ArrayList<>();
+        for (int i = 0;i < word.length(); i++){
+            letters.add(String.valueOf(word.charAt(i)));
+        }
+
+        for (String letter : letters) {
+            if (!guessedLetters.contains(letter)) {
+                gameIsWon = false;
+                break;
+            }
+        }
+        return gameIsWon;
+    }
+
+    private void gameWon() {
+        System.out.println("Wygrałeś");
+    }
+
+   private boolean checkIfIsLetter(String letter) {
+        return letter.length() == 1;
+   }
+
+   private String askForInput() {
+       Scanner myObj = new Scanner(System.in);
+       System.out.println("");
+       System.out.println("Podaj literę");
+
+       String letter = myObj.nextLine();
+
+       while(!checkIfIsLetter(letter)) {
+           System.out.println("Litera niepoprawna");
+           System.out.println("Podaj literę");
+
+           letter = myObj.nextLine();
+       }
+
+       return letter.toUpperCase(Locale.ROOT);
+   }
+    private void printState(){
+       System.out.println(HANGMANPICS[counter]);
+       System.out.println("#### Słowo: ");
+
+        ArrayList<String> letters = new ArrayList<>();
+        for (int i = 0;i < word.length(); i++){
+            letters.add(String.valueOf(word.charAt(i)));
+        }
+
+        for (String letter : letters) {
+            if (guessedLetters.contains(letter)) {
+                System.out.print(letter);
+            } else {
+                System.out.print(" _ ");
+            }
+        }
+    }
 }
