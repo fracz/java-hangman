c4e6c7dcd912a2549bddbef06383099cbeb7cd20
jachera <pjachera@gmail.com>
2024-02-04 11:10:08 +0100

    First Main function

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.hangman;
 
+import java.util.Scanner;
+
 public class Hangman {
 
     public static final String[] HANGMANPICS = new String[]{
@@ -59,6 +61,21 @@ public class Hangman {
     public static final String GAMEWIN = "(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧";
 
     public static void main(String[] args) {
+
+
+        RandomWord random = new RandomWord();
+        Logic logic = new Logic(random.getRandomWord());
+        String input;
+
+        do{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Podaj literke : ");
+            input = scanner.nextLine();
+
+
+
+        }while(logic.checkLetter(input.charAt(0)));
+
 
         PrintResult.printResult(6, "jola");
 
