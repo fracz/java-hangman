b4e51733fbcae8fcc883fcc176c6f89356009b71
Ania <listopad@agh.edu.pl>
2025-02-09 10:28:09 +0100

    add_UserInput

--- src/main/java/pl/edu/agh/hangman/UserInput.java
+++ src/main/java/pl/edu/agh/hangman/UserInput.java
@@ -1,5 +1,13 @@
 package pl.edu.agh.hangman;
 
+import java.util.Scanner;
+
 //Obsługuje wejście użytkownika, np. zgadywanie liter.
 public class UserInput {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public char guess() {
+        System.out.print("Podaj literę: ");
+        return scanner.next().toUpperCase().charAt(0);
+    }
 }
