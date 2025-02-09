c5387d0806a39122a857300eada6952431b7603f
Lulily13 <bloodyakuma13@yahoo.com>
2025-02-09 10:09:36 +0100

    Set up Main class to start the application

--- src/main/java/pl/edu/agh/hangman/Main.java
+++ src/main/java/pl/edu/agh/hangman/Main.java
@@ -3,5 +3,6 @@ package pl.edu.agh.hangman;
 //Punkt startowy aplikacji, inicjalizuje grę.
 public class Main {
     public static void main(String[] args) {
+        new HangmanGame().start();
     }
 }
