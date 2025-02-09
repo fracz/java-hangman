df026133cfca966ade500a0346dc771db7cd60d0
Lulily13 <bloodyakuma13@yahoo.com>
2025-02-09 10:07:06 +0100

    Add GameState class

--- src/main/java/pl/edu/agh/hangman/GameState.java
+++ src/main/java/pl/edu/agh/hangman/GameState.java
@@ -1,5 +1,45 @@
 package pl.edu.agh.hangman;
 
+import java.util.HashSet;
+import java.util.Set;
+
 //Przechowuje stan gry, zgadywane litery, liczbę prób, wygrane/przegrane.
 public class GameState {
+    private final String word;
+    private final Set<Character> guessedLetters = new HashSet<>();
+    private int attemptsLeft = 6;
+
+    public GameState(String word) {
+        this.word = word;
+    }
+
+    public boolean isGameOver() {
+        return attemptsLeft == 0 || getMaskedWord().equals(word);
+    }
+
+    public void processGuess(char guess) {
+        if (!word.contains(String.valueOf(guess))) {
+            attemptsLeft--;
+        }
+        guessedLetters.add(guess);
+    }
+
+    public String getMaskedWord() {
+        StringBuilder maskedWord = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            maskedWord.append(guessedLetters.contains(c) ? c : '_');
+            maskedWord.append(' ');
+        }
+        return maskedWord.toString().trim();
+    }
+
+    public int getAttemptsLeft() {
+        return attemptsLeft;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
+
 }
