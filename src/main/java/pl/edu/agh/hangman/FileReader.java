be5efdb70bf660b9b682bd5a198450650d5eff31
mbalicki <mateusz.balick96@gmail.com>
2026-01-31 10:47:03 +0100

    ReadFile class - filePath refactor

--- src/main/java/pl/edu/agh/hangman/FileReader.java
+++ src/main/java/pl/edu/agh/hangman/FileReader.java
@@ -2,15 +2,17 @@ package pl.edu.agh.hangman;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class FileReader {
 
-    private String filePath = "/home/students/m/b/mbalicki/Documents/java-hangman/src/main/resources/slowa.txt";
+    private String filePath = "src/main/resources/slowa.txt";
+
 
     public ArrayList<String> scanFile() throws FileNotFoundException {
-
         ArrayList<String> words = new ArrayList<>();
         File file = new File(filePath);
 
@@ -21,5 +23,10 @@ public class FileReader {
             }
         }
         return words;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader fileReader = new FileReader();
+        System.out.println(fileReader.scanFile());
     }
 }
