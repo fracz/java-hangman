69b7b589044e8398da0f2552fdac273c99d0e725
Aleksandra <aleksandra.paperz@email.com>
2026-01-31 10:56:50 +0100

    3

--- src/main/java/pl/edu/agh/hangman/UserInput.java
+++ src/main/java/pl/edu/agh/hangman/UserInput.java
@@ -1,16 +1,18 @@
 package pl.edu.agh.hangman;
+
 import java.util.Scanner;
 
 public class UserInput {
+
     private Scanner scanner;
 
     public UserInput() {
-        scanner = new Scanner (System.in);
+        scanner = new Scanner(System.in);
     }
 
     public void showWelcomeMessage() {
         System.out.println("==========================");
-        System.out.println("   WITAJ W GRZE HANGMAN"   );
+        System.out.println("   WITAJ W GRZE HANGMAN   ");
         System.out.println("==========================");
         System.out.println("Zgaduj litery, aby odgadnąć hasło.");
         System.out.println();
@@ -19,8 +21,13 @@ public class UserInput {
     public char getLetter() {
         while (true) {
             System.out.print("Podaj literę: ");
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().toLowerCase();
 
+            if (input.length() == 1 && Character.isLetter(input.charAt(0))) {
+                return input.charAt(0);
+            }
+
+            System.out.println("Błąd — wpisz jedną literę.");
         }
     }
 }
