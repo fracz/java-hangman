6e6b047cb6f4abd639876d87b93d529c347bd3e4
Michał Huzior <mhuzior@student.agh.edu.pl>
2026-01-31 12:47:16 +0100

    new impementation to gameLoop

--- src/main/java/pl/edu/agh/hangman/GameLoop.java
+++ src/main/java/pl/edu/agh/hangman/GameLoop.java
@@ -2,10 +2,16 @@ package pl.edu.agh.hangman;
 
 import java.util.Scanner;
 
+import static jdk.jfr.internal.test.DeprecatedMethods.counter;
+
 public class GameLoop {
+
     public GameLoop() {
         boolean flag = false;
+        int counter = 0;
+        drawHangman(counter);
         do{
+
             Scanner input = new Scanner(System.in);
             System.out.println("Podaj znak:");
             char nextChar = input.next().charAt(0);
@@ -14,6 +20,9 @@ public class GameLoop {
 
             if(machSentence == userSentence){
                 flag=true;
+            }else{
+                counter++;
+                drawHangman(counter);
             }
         }while(flag == flase);
 
