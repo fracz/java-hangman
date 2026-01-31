0631050d066a67764ab6e56bfd90cc7b8e356d71
guzikpaula <guzikpaula@gmail.com>
2026-01-31 10:41:56 +0100

    Class Word first version, output as a String (Word) with upper case

---
+++ src/main/java/pl/edu/agh/hangman/Word.java
@@ -0,0 +1,40 @@
+package pl.edu.agh.hangman;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+public class Word {
+
+    private List<String> slowa;
+    private Random random = new Random();
+
+    public Word(String nazwaPliku) throws IOException {
+        slowa = Files.readAllLines(Path.of(nazwaPliku));
+
+        // usuwanie NBSP
+        for (int i = 0; i < slowa.size(); i++) {
+            slowa.set(i, slowa.get(i)
+                    .replace('\u00A0', ' ')
+                    .trim());
+        }
+
+        // zmiana na UPPER CASE (z PL znakami)
+        for (int i = 0; i < slowa.size(); i++) {
+            slowa.set(i, slowa.get(i)
+                    .toUpperCase(Locale.forLanguageTag("pl")));
+        }
+
+        slowa.removeIf(String::isBlank);
+    }
+
+    public String losujSlowo() {
+        if (slowa.isEmpty()) {
+            throw new IllegalStateException("Brak słów w pliku!");
+        }
+        return slowa.get(random.nextInt(slowa.size()));
+    }
+}
