d9282d3eeb458c5b2a85e9ec2e0a60f75c3c9e5d
mbalicki <mateusz.balick96@gmail.com>
2026-01-31 09:58:12 +0100

    ReadFile class

---
+++ src/main/java/pl/edu/agh/hangman/ReadFile.java
@@ -0,0 +1,22 @@
+package pl.edu.agh.hangman;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReadFile {
+    private String filePath = "/home/students/m/b/mbalicki/Documents/java-hangman/src/main/resources/slowa.txt";
+
+    public ArrayList<String> scanFile() throws FileNotFoundException {
+        ArrayList<String> words = new ArrayList<>();
+        File file = new File(filePath);
+        try (Scanner fileReader = new Scanner(file)) {
+            while (fileReader.hasNextLine()) {
+                String word = fileReader.nextLine();
+                words.add(word);
+            }
+        }
+        return words;
+    }
+}
